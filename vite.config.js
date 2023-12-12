f1bc7633a418e5d8dcddc098b5ff0c5a893f7ef2
93amrkakashi <93amr.kakashi@gmail.com>
2023-12-12 13:55:56 -0800

    xx

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 // vite.config.js
 import dotenv from 'dotenv';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
+
 
 export default ({ mode }) => {
   const env = dotenv.config().parsed;
 
   return {
 
-    css: {
-      postcss: {
-        plugins: [
-          tailwindcss,
-          autoprefixer,
-        ],
-      },
-    },
+  
     define: {
       'process.env': process.env,
     },
